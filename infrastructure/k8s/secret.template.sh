e1334815d0bf4c4626e6048c30fc63262e85069f
tajpouria <tajpouria.dev@gmail.com>
2020-07-23 01:48:17 +0430

    Fixed -- Codacy issues

--- infrastructure/k8s/secret.template.sh
+++ infrastructure/k8s/secret.template.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 kubectl create secret generic stub-secret --from-literal=GOOGLE_CLIENT_ID=XXX --from-literal=XXX --from-literal=JWT_SECRET=XXX --from-literal=MAILER='{"service": "gmail", "auth": {"user": "XXX", "pass": "XXX"}}'
